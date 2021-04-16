66e823d613bf181020f57c70103450b9cbc463c8
Riyu <68317883+riyukuro@users.noreply.github.com>
2021-04-16 06:55:03 -0500

    Update HakuNeko-Connectors-Keys.py

--- HakuNeko-Connectors-Keys.py
+++ HakuNeko-Connectors-Keys.py
@@ -8,15 +8,12 @@ def connectors(directory):
         with open(directory + file, 'r') as f:
             for line in f:
                 if 'super.id =' in line:
-                    combined = {'Connector': '', 'Key': ''}
-                    combined['Connector'] = str(os.path.basename(file)).replace('.mjs', '') #os.path.splitext(file)[0]
                     x = str(line).replace('        super.id = "', '').replace("        super.id = '", "").replace("';\n", '').replace('";\n', '')
-                    combined['Key'] = x
-                    matched.append(combined)
+                    matched.append(x)
 
-    with open('HN_Connector_Key_list.json', 'w') as file:
+    with open('Hakuneko-connector-list.json', 'w') as file:
         json.dump(matched, file, indent=4)
 
-directory = '<Insert Hakuneko Connectors Folder Here>'
+directory = '<Insert Directory Here>'
 
 connectors(directory)
