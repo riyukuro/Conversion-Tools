de2d8d8063f168a32b32d0fef3a8f1e3130b13ae
Riyu <68317883+riyukuro@users.noreply.github.com>
2021-05-05 21:12:13 -0500

    Add files via upload
    Add hakuneko DB converter for legacy mangadex ids to new UUIDs.

---
+++ HN-Mangadex-Legacy-Converter.py
@@ -0,0 +1,30 @@
+import json
+import requests
+
+def main():
+    legacy_mapping = 'https://api.mangadex.org/legacy/mapping'
+
+    hakuneko =  open('hakuneko.bookmarks', 'r+')
+    hakuneko_data = json.load(hakuneko)
+    hakuneko.close()
+
+    for i in hakuneko_data:
+        if i['key']['connector'] != 'mangadex':
+            continue
+        else:
+            mangaid = int(i['key']['manga'].replace('/', '').replace('manga', ''))
+            data = {"type": "manga", "ids": [mangaid]}
+            
+            post_data = requests.post(legacy_mapping, json=data)
+            f = json.loads(post_data.text)
+            for z in f:
+                new_id = str(z['data']['attributes']['newId'])
+
+            i['key']['manga'] = f'/manga/{new_id}'
+            print(i['key'])
+
+    hakuneko = open('hakuneko.bookmarks', 'w')
+    json.dump(hakuneko_data, hakuneko)
+    hakuneko.close()
+
+main()
